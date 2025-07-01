1c902755becf89322c6b56002cb7dd11c4547d3d
Munzir Azmi <ahmedmunzir98@gmail.com>
2025-07-01 22:49:47 +0530

    Create outputs.tf

---
+++ modules/vpc/outputs.tf
@@ -0,0 +1,12 @@
+output "public_subnet_id" {
+  description = "The ID of the public subnet"
+  value       = aws_subnet.public_subnet.id
+}
+output "private_subnet_id" {
+  description = "The ID of the private subnet"
+  value       = aws_subnet.private_subnet.id
+}
+output "security_group_id" {
+  description = "The ID of the security group"
+  value       = aws_security_group.allow_all.id
+}
